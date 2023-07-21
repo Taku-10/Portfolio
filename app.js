c786151e3ef8e815403bd92d96c5ce35d6627675
Takudzwa <takudzwamaseva020@gmail.com>
2023-07-21 19:11:20 +0200

    Search Engine Optimization

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 if (process.env.NODE_ENV !== "production") {
-    require("dotenv").config();
-  }
+  require("dotenv").config();
+}
 
 const express = require("express");
 const app = express();
@@ -17,19 +17,20 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
 
-
-app.use(session({
-  name: "session",
-  secret: "ILoveBiscuits",
-  resave: false,
-  saveUninitialized: true,
-  cookie: {
-    httpOnly: true,
-    // secure: true,
-    expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
-    maxAge: 1000 * 60 * 60 * 24 * 7,
-  }
-}));
+app.use(
+  session({
+    name: "session",
+    secret: "ILoveBiscuits",
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+      httpOnly: true,
+      // secure: true,
+      expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+      maxAge: 1000 * 60 * 60 * 24 * 7,
+    },
+  })
+);
 
 app.use(flash());
 app.use((req, res, next) => {
@@ -40,47 +41,45 @@ app.use((req, res, next) => {
 });
 
 app.get("/", (req, res) => {
-    res.render("home.ejs");
-})
+  res.render("home.ejs");
+});
 
 app.get("/about-me", (req, res) => {
   res.render("about.ejs");
-})
+});
 
 app.get("/contact-me", (req, res) => {
   res.render("contact.ejs");
-})
+});
 
 app.get("/skills", (req, res) => {
   res.render("skills.ejs");
-})
+});
 
 app.get("/projects", (req, res) => {
   res.render("projects.ejs");
-})
+});
 
-app.post("/contact-me", async(req, res) => {
-
-    const{name, email, message} = req.body;
-    const msg = {
-      to: process.env.EMAIL_FROM, 
-      from: process.env.EMAIL_FROM,
-      replyTo: email,
-      subject: `New contact submission from ${name}`,
-      html: `
+app.post("/contact-me", async (req, res) => {
+  const { name, email, message } = req.body;
+  const msg = {
+    to: process.env.EMAIL_FROM,
+    from: process.env.EMAIL_FROM,
+    replyTo: email,
+    subject: `New contact submission from ${name}`,
+    html: `
       <p><strong>Name:</strong> ${name}</p>
       <p><strong>Email:</strong> ${email}</p>
       <p><strong>Message:</strong> ${message}</p>
     `,
-    }
+  };
 
-    await sgMail.send(msg);
-    req.flash("success", "Your message has been sent successfully!")
-    res.redirect('/contact-me');
+  await sgMail.send(msg);
+  req.flash("success", "Your message has been sent successfully!");
+  res.redirect("/contact-me");
+});
 
-  })
-
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
-    console.log(`Serving on port ${port}`);
+  console.log(`Serving on port ${port}`);
 });
